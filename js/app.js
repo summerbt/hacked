011d567e6dfbcade4403a4bdae9307751a7404b3
summertime <summerbartontaylor@gmail.com>
2015-12-02 14:07:59 -0600

    added meta viewport

--- js/app.js
+++ js/app.js
@@ -8,13 +8,26 @@ function launchTerminal(){
 			location.reload();
 		}
 	});
-	$(document).on('keydown',function(key){
+
+	$("#gameTerminal").on('keydown',function(key){
+		if (key.keyCode == 13){
+			$("#gameTerminal").hide(1000);
+		}
+	});
+
+	$("#introBanner").on('keydown',function(key){
 		if (key.keyCode == 13){
 			$("#introBanner").hide(1000);
 		}
 	});
 
-
+	$("#menu").mouseup(function(e){
+		var game = $("#gameTerminal");
+		if(e.target.id != game.attr('id') && ! game.has(e.target).length)
+        {
+            game.hide(1000);
+        }
+	});
 
 	
 	$("#fictionBanner").on('click',launchTerminal);
